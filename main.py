bdb1b10f16388b071cd56a093f17665ddb43c0d1
pendragonarthur <Arthur Quaresma>
2024-10-21 22:00:50 -0300

    minor fixes

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pyaudio 
- # mic = index 1
+import vosk
+import json
+import subprocess
+
+MODEL_PATH = "model/vosk"
+model = vosk.Model(MODEL_PATH)
 
 CHANNELS = 1
 FRAME_RATE = 16000
@@ -9,14 +14,29 @@ SAMPLE_SIZE = 2
 
 def record_mic(chunk=1024):
     p = pyaudio.PyAudio()
-
+    r = vosk.KaldiRecognizer(model, FRAME_RATE)
     stream = p.open(format=AUDIO_FORMAT, channels=CHANNELS, rate=FRAME_RATE, input=True, input_device_index=1, frames_per_buffer=chunk)
     print("I'm listening...")
     frames = []
-    for _ in range(0, int(FRAME_RATE * DURATION / chunk)):
-        data = stream.read(chunk)
-        frames.append(data)
-    print("Good talking!")
 
-def transcribe_audio():
-    pass
+    try:
+        while True:
+            data = stream.read(chunk)
+
+            if r.AcceptWaveform(data):
+                result = r.Result()
+                text = json.loads(result)["text"]
+                # TODO: baixar recasepunc 
+                # cased = subprocess.check_output("python recasepunc/recasepunc.py predict recasepunc/checkpoint", text=True, input=text)
+                
+            
+
+    except KeyboardInterrupt:
+        print("Stream terminated.")
+    
+    finally:
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+record_mic()
