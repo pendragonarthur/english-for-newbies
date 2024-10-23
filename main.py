1665fdbc1ec407b27d758f21b7cd37017bc85961
pendragonarthur <Arthur Quaresma>
2024-10-23 20:26:49 -0300

    feat: add llama model

--- main.py
+++ main.py
@@ -1,42 +1,61 @@
 import pyaudio 
 import vosk
 import json
-import subprocess
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from llama_cpp import Llama
 
-MODEL_PATH = "model/vosk"
-model = vosk.Model(MODEL_PATH)
+load_dotenv()
 
 CHANNELS = 1
 FRAME_RATE = 16000
 DURATION = 20
 AUDIO_FORMAT = pyaudio.paInt16
 SAMPLE_SIZE = 2
+MODEL_PATH = "/home/arthur/dev/python-projects/english-for-newbies/model/vosk-model-en-us-0.22/vosk-model-en-us-0.22"
+API_KEY = os.getenv("OPENAI_API_KEY")
 
-def record_mic(chunk=1024):
+
+model = vosk.Model(MODEL_PATH)
+r = vosk.KaldiRecognizer(model, FRAME_RATE)
+llm = Llama() #TODO: implement llama
+
+
+def get_user_audio(chunk=1024):
     p = pyaudio.PyAudio()
-    r = vosk.KaldiRecognizer(model, FRAME_RATE)
     stream = p.open(format=AUDIO_FORMAT, channels=CHANNELS, rate=FRAME_RATE, input=True, input_device_index=1, frames_per_buffer=chunk)
     print("I'm listening...")
-    frames = []
 
     try:
+        text = ""
         while True:
             data = stream.read(chunk)
 
             if r.AcceptWaveform(data):
                 result = r.Result()
                 text = json.loads(result)["text"]
-                # TODO: baixar recasepunc 
                 # cased = subprocess.check_output("python recasepunc/recasepunc.py predict recasepunc/checkpoint", text=True, input=text)
+                print(text)
+                return text
                 
-            
-
     except KeyboardInterrupt:
         print("Stream terminated.")
-    
+
     finally:
         stream.stop_stream()
         stream.close()
         p.terminate()
 
-record_mic()
+def get_model_answer(user_input):
+    #TODO: add logic
+    pass
+
+def initiate_conversation():
+    while True:
+        speech = get_user_audio()
+
+        answer = get_model_answer(speech)
+        print(answer)
+
+initiate_conversation()
